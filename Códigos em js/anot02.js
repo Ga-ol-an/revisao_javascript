425b48b78f7cc0f6d6454e7e302e0ffcf25f802d
Gabriel Andrade <gabrielolandrade@hotmail.com.br>
2020-04-30 11:34:14 -0300

    Add files via upload

---
+++ Códigos em js/anot02.js
@@ -0,0 +1,39 @@
+// é o comente de 1 ilnha
+/* comentário de 
+varias linhas */ 
+/*  AULA 5 : ANOTAÇÕES
+Aqui existe o sinla de = e == igual em C
+Existe o null aqui tbm (nada)[
+    Em JS, voce póde usar 3 aspas: "",'', `crase`, porém, há diferenças
+Variáveis:
+O nome da variável é Identifacador:
+    *Podem começar com letra, $ ou _
+    *Não pode começar com numero
+    *Pode usar acento e símbolo
+    *Não pode conter espaços
+    *Não pode ser plavras reservadas
+
+    pra abiri um terminal pra treino, vc ppode ir no Nodejs ou simplesmente 
+    ir em terminar la encima e apertar em novo termina ( ou crtl+shift+´)
+    aí vc digita node no terminal
+    ctrl+l limpra a tela
+Dicas pro nome da variavel:
+    *Maiscula e minusicula tme diferença
+    *Nomes coerentes
+As variaveis de JS são:
+number
+    tem 2:
+        infinity
+        NaN (not in number)
+string
+boolean
+null
+undefined
+object 
+function
+<!--Tudo que tá em style é css 
+        Tudo que tá em script é java script-->
+
+ <!-- Em js, pra vc ver o tipo da variável vc escreve typeof
+    Voce noa precisa ficar escrevendo int blabla, vc pode fazer igual em python-->
+*/
---
+++ Códigos em js/anot08.js
@@ -0,0 +1,3 @@
+//No js puro, pra escrever na tela vc usa console. log
+// e nao o document.write
+console.log("o console funcionou corretamente\n")
